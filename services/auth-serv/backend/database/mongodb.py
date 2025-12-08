16e13b78db5b4732e553f3574373afa7404881cd
Younes Bazzaoui <younrun.rocka@gmail.com>
2025-12-08 14:44:26 +0100

    connected to mangodb atlas so that we all members can have the same database

--- services/auth-serv/backend/database/mongodb.py
+++ services/auth-serv/backend/database/mongodb.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 # Load from environment variables (set by Docker)
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL", "44")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "datagov")
 
 client = AsyncIOMotorClient(MONGO_URL)
