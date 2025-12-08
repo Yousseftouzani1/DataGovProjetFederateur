70dcf1324b3f41414fc5e0f39d823c5c0b405e85
Younes Bazzaoui <younrun.rocka@gmail.com>
2025-12-08 02:55:42 +0100

    updated the gitignore

--- backend/database/mongodb.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URL = "mongodb://localhost:27017/"
-DATABASE_NAME = "datagov"
-
-client = AsyncIOMotorClient(MONGO_URL)
-db = client[DATABASE_NAME]
---
+++ services/auth-serv/backend/database/mongodb.py
@@ -0,0 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+# Load from environment variables (set by Docker)
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "datagov")
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client[DATABASE_NAME]
